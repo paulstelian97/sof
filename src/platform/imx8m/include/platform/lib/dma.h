bbb64de1d275a9c0282008cec42774c68d985939
Paul Olaru <paul.olaru@nxp.com>
2020-03-13 11:19:36 +0200

    Correct setting for ACR bit
    Previous setting caused hangs on ARM side.
    
    Signed-off-by: Paul Olaru <paul.olaru@nxp.com>

--- src/platform/imx8m/include/platform/lib/dma.h
+++ src/platform/imx8m/include/platform/lib/dma.h
@@ -30,7 +30,7 @@
 #define SDMA2_IRQ	7 /* TODO What? */
 #define SDMA2_IRQ_NAME	"irqstr2" /* TODO find the correct one */
 
-#define SDMA_CORE_RATIO 1/* Enable ACR bit as it's needed for this platform */
+#define SDMA_CORE_RATIO 0/* Enable ACR bit as it's needed for this platform */
 
 #endif /* __PLATFORM_LIB_DMA_H__ */
 
